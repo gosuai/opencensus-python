6b0df49a4b195372c049891cb8920d49ab37ad2e
Eric Hauser <ewhauser@gmail.com>
2018-12-24 11:56:51 -0800

    add test for tracing across threadpools

--- tests/unit/trace/ext/tornado/test_tornado_integration.py
+++ tests/unit/trace/ext/tornado/test_tornado_integration.py
@@ -1,6 +1,8 @@
 import pytest
 import tornado
+from concurrent import futures
 from tornado import web
+from tornado.concurrent import run_on_executor
 from tornado.httpclient import HTTPClientError
 
 from opencensus.trace import execution_context as ec, config_integration
@@ -37,6 +39,24 @@ class ScopeHandler(tornado.web.RequestHandler):
         self.write('{}')
 
 
+class ThreadPoolHandler(tornado.web.RequestHandler):
+    executor = futures.ThreadPoolExecutor(max_workers=1)
+
+    @run_on_executor
+    def do_something(self, span):
+        tracer = ec.get_opencensus_tracer()
+        with tracer.start_span('child') as child_span:
+            assert span.span_id == child_span.parent_span.span_id
+            yield tornado.gen.sleep(0.0)
+
+    @tornado.gen.coroutine
+    def get(self):
+        span = ec.get_current_span()
+        assert span is not None
+        yield self.do_something(span)
+        assert ec.get_current_span() is span
+        self.write('{}')
+
 @pytest.fixture
 def app():
     settings = {}
@@ -44,7 +64,8 @@ def app():
         [
             ('/', MainHandler),
             ('/error', ErrorHandler),
-            ('/coroutine_scope', ScopeHandler),
+            ('/coroutine', ScopeHandler),
+            ('/threadpool', ThreadPoolHandler),
         ],
         **settings
     )
@@ -59,7 +80,13 @@ def test_hello_world(http_client, base_url):
 
 @pytest.mark.gen_test
 def test_coroutine(http_client, base_url):
-    response = yield http_client.fetch(base_url + '/coroutine_scope')
+    response = yield http_client.fetch(base_url + '/coroutine')
+    assert response.code == 200
+
+
+@pytest.mark.gen_test
+def test_threadpool(http_client, base_url):
+    response = yield http_client.fetch(base_url + '/threadpool')
     assert response.code == 200
 
 
