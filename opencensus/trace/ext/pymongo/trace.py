23e980b2b989b9927590db7261c454539b1954ce
Eric Hauser <ewhauser@gmail.com>
2018-12-25 15:24:12 -0800

    support for pymongo

---
+++ opencensus/trace/ext/pymongo/trace.py
@@ -0,0 +1,116 @@
+# Copyright 2017, OpenCensus Authors
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+import json
+import logging
+
+import six
+from pymongo import monitoring
+
+from opencensus.trace import execution_context
+from opencensus.trace import span as span_module
+
+log = logging.getLogger(__name__)
+
+MODULE_NAME = 'pymongo'
+
+
+def trace_integration(tracer=None):
+    """Wrap the pymongo connection to trace it."""
+    log.info('Integrated module: {}'.format(MODULE_NAME))
+    monitoring.register(CommandTracer())
+
+
+class CommandTracer(monitoring.CommandListener):
+
+    def started(self, event):
+        tracer = execution_context.get_opencensus_tracer()
+        span = tracer.start_span('{}.cmd'.format(MODULE_NAME))
+        span.span_kind = span_module.SpanKind.CLIENT
+        tracer.add_attribute_to_current_span('{}.db'.format(MODULE_NAME), event.database_name)
+        _set_query_metadata(event)
+
+    def succeeded(self, event):
+        tracer = execution_context.get_opencensus_tracer()
+        tracer.end_span()
+
+    def failed(self, event):
+        tracer = execution_context.get_opencensus_tracer()
+        tracer.end_span()
+
+# Borrowed from https://github.com/DataDog/dd-trace-py/blob/master/ddtrace/contrib/pymongo/parse.py
+
+def _set_query_metadata(event):
+    tracer = execution_context.get_opencensus_tracer()
+    name, db, coll, query = _parse_spec(event.command)
+    tracer.add_attribute_to_current_span('{}.collection'.format(MODULE_NAME), coll)
+    if query:
+        nq = _normalize_filter(query)
+        q = json.dumps(nq)
+        value = '{} {} {}'.format(name, coll, q)
+    else:
+        value = '{} {}'.format(name, coll)
+    tracer.add_attribute_to_current_span('{}.query'.format(MODULE_NAME), value)
+
+
+def _parse_spec(spec, db=None):
+    """ Return a Command that has parsed the relevant detail for the given
+        pymongo SON spec.
+    """
+    items = list(spec.items())
+    if not items:
+        return None
+    name, coll = items[0]
+
+    query = None
+    if name == 'update':
+        updates = spec.get('updates')
+        if updates:
+            query = updates[0].get("q")
+
+    elif name == 'find':
+        filter = spec.get('filter')
+        if filter:
+            query = filter
+
+    elif name == 'delete':
+        dels = spec.get('deletes')
+        if dels:
+            query = dels[0].get("q")
+
+    return name, db, coll, query
+
+
+def _normalize_filter(f=None):
+    if f is None:
+        return {}
+    elif isinstance(f, list):
+        # normalize lists of filters
+        # e.g. {$or: [ { age: { $lt: 30 } }, { type: 1 } ]}
+        return [_normalize_filter(s) for s in f]
+    elif isinstance(f, dict):
+        # normalize dicts of filters
+        #   {$or: [ { age: { $lt: 30 } }, { type: 1 } ]})
+        out = {}
+        for k, v in six.iteritems(f):
+            if k == "$in" or k == "$nin":
+                # special case $in queries so we don't loop over lists.
+                out[k] = "?"
+            elif isinstance(v, list) or isinstance(v, dict):
+                out[k] = _normalize_filter(v)
+            else:
+                out[k] = '?'
+        return out
+    else:
+        return {}
+
---
+++ tests/unit/trace/ext/pymongo/test_pymongo.py
@@ -0,0 +1,242 @@
+import time
+
+import pymongo
+import pytest
+
+from opencensus.trace import config_integration
+from opencensus.trace import tracer as tracer_module
+from opencensus.trace.exporters.base import Exporter
+from opencensus.trace.ext.pymongo.trace import _normalize_filter
+from opencensus.trace.propagation.google_cloud_format import GoogleCloudFormatPropagator
+from opencensus.trace.samplers import AlwaysOnSampler
+
+config_integration.trace_integrations(['pymongo'])
+
+
+class CapturingExporter(Exporter):
+
+    def __init__(self):
+        super(CapturingExporter, self).__init__()
+        self._spans = []
+
+    @property
+    def spans(self):
+        return self._spans
+
+    def emit(self, span_datas):
+        pass
+
+    def export(self, span_datas):
+        self._spans.append(span_datas)
+
+
+@pytest.fixture(autouse=True)
+def exporter():
+    exporter = CapturingExporter()
+    tracer_module.Tracer(
+        sampler=AlwaysOnSampler(),
+        exporter=exporter,
+        propagator=GoogleCloudFormatPropagator()
+    )
+    return exporter
+
+
+@pytest.fixture(autouse=True)
+def client():
+    return pymongo.MongoClient(port=27017)
+
+
+def test_normalize_filter():
+    cases = [
+        (None, {}),
+        (
+            {'team': 'leafs'},
+            {'team': '?'},
+        ),
+        (
+            {'age': {'$gt': 20}},
+            {'age': {'$gt': '?'}},
+        ),
+        (
+            {'age': {'$gt': 20}},
+            {'age': {'$gt': '?'}},
+        ),
+        (
+            {'_id': {'$in': [1, 2, 3]}},
+            {'_id': {'$in': '?'}},
+        ),
+        (
+            {'_id': {'$nin': [1, 2, 3]}},
+            {'_id': {'$nin': '?'}},
+        ),
+
+        (
+            20,
+            {},
+        ),
+        (
+            {
+                'status': 'A',
+                '$or': [{'age': {'$lt': 30}}, {'type': 1}],
+            },
+            {
+                'status': '?',
+                '$or': [{'age': {'$lt': '?'}}, {'type': '?'}],
+            },
+        ),
+    ]
+    for i, expected in cases:
+        out = _normalize_filter(i)
+        assert expected == out
+
+
+def test_update(client, exporter):
+    # ensure we trace deletes
+    db = client['testdb']
+    db.drop_collection('songs')
+    input_songs = [
+        {'name': 'Powderfinger', 'artist': 'Neil'},
+        {'name': 'Harvest', 'artist': 'Neil'},
+        {'name': 'Suzanne', 'artist': 'Leonard'},
+        {'name': 'Partisan', 'artist': 'Leonard'},
+    ]
+    db.songs.insert_many(input_songs)
+
+    result = db.songs.update_many(
+        {'artist': 'Neil'},
+        {'$set': {'artist': 'Shakey'}},
+    )
+
+    eq_(result.matched_count, 2)
+    eq_(result.modified_count, 2)
+
+    # ensure all is traced.
+    spans = exporter.spans
+    queries = []
+    for span_list in spans:
+        for span in span_list:
+            # ensure all the of the common attributesdata is set
+            eq_(span.attributes.get('pymongo.collection'), 'songs')
+            eq_(span.attributes.get('pymongo.db'), 'testdb')
+            queries.append(span.attributes.get('pymongo.query'))
+
+    expected_resources = ['drop songs', 'insert songs', 'update songs {"artist": "?"}']
+    assert expected_resources == queries
+
+
+def test_delete(client, exporter):
+    # ensure we trace deletes
+    db = client['testdb']
+    collection_name = 'here.are.songs'
+    db.drop_collection(collection_name)
+    input_songs = [
+        {'name': 'Powderfinger', 'artist': 'Neil'},
+        {'name': 'Harvest', 'artist': 'Neil'},
+        {'name': 'Suzanne', 'artist': 'Leonard'},
+        {'name': 'Partisan', 'artist': 'Leonard'},
+    ]
+
+    songs = db[collection_name]
+    songs.insert_many(input_songs)
+
+    # test delete one
+    af = {'artist': 'Neil'}
+    eq_(songs.count(af), 2)
+    songs.delete_one(af)
+    eq_(songs.count(af), 1)
+
+    # test delete many
+    af = {'artist': 'Leonard'}
+    eq_(songs.count(af), 2)
+    songs.delete_many(af)
+    eq_(songs.count(af), 0)
+
+    # ensure all is traced.
+    spans = exporter.spans
+    queries = []
+    for span_list in spans:
+        for span in span_list:
+            # ensure all the of the common attributesdata is set
+            eq_(span.attributes.get('pymongo.collection'), collection_name)
+            eq_(span.attributes.get('pymongo.db'), 'testdb')
+            queries.append(span.attributes.get('pymongo.query'))
+
+    expected_resources = [
+        'drop here.are.songs',
+        'insert here.are.songs',
+        'count here.are.songs',
+        'delete here.are.songs {"artist": "?"}',
+        'count here.are.songs',
+        'count here.are.songs',
+        'delete here.are.songs {"artist": "?"}',
+        'count here.are.songs',
+    ]
+
+    assert expected_resources == queries
+
+
+def test_insert_find(client, exporter):
+    db = client.testdb
+    db.drop_collection('teams')
+    teams = [
+        {
+            'name': 'Toronto Maple Leafs',
+            'established': 1917,
+        },
+        {
+            'name': 'Montreal Canadiens',
+            'established': 1910,
+        },
+        {
+            'name': 'New York Rangers',
+            'established': 1926,
+        }
+    ]
+
+    # create some data (exercising both ways of inserting)
+
+    db.teams.insert_one(teams[0])
+    db.teams.insert_many(teams[1:])
+
+    # wildcard query (using the [] syntax)
+    cursor = db['teams'].find()
+    count = 0
+    for row in cursor:
+        count += 1
+    eq_(count, len(teams))
+
+    # scoped query (using the getattr syntax)
+    q = {'name': 'Toronto Maple Leafs'}
+    queried = list(db.teams.find(q))
+    eq_(len(queried), 1)
+    eq_(queried[0]['name'], 'Toronto Maple Leafs')
+    eq_(queried[0]['established'], 1917)
+
+    spans = exporter.spans
+    queries = []
+    for span_list in spans:
+        for span in span_list:
+            # ensure all the of the common attributesdata is set
+            eq_(span.attributes.get('pymongo.collection'), 'teams')
+            eq_(span.attributes.get('pymongo.db'), 'testdb')
+            queries.append(span.attributes.get('pymongo.query'))
+
+    expected_resources = [
+        'drop teams',
+        'insert teams',
+        'insert teams',
+    ]
+
+    # query names should be used in >3.1
+    name = 'find' if pymongo.version_tuple >= (3, 1, 0) else 'query'
+
+    expected_resources.extend([
+        '{} teams'.format(name),
+        '{} teams {{"name": "?"}}'.format(name),
+    ])
+
+    eq_(expected_resources, queries)
+
+
+def eq_(first, second):
+    assert first == second
