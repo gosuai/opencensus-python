6654352d9f2c1c3dd519318f374b7766ad2b2149
Eric Hauser <ewhauser@gmail.com>
2019-01-01 13:35:38 -0800

    pass context along thread boundaries for motor

--- opencensus/trace/ext/pymongo_tornado/trace.py
+++ opencensus/trace/ext/pymongo_tornado/trace.py
@@ -1,41 +1,77 @@
-# Copyright 2017, OpenCensus Authors
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#     http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
 import logging
 
+from concurrent import futures
 from pymongo import monitoring
-from tornado.ioloop import IOLoop
 
+from opencensus.trace import execution_context, tracer
 from opencensus.trace.ext.pymongo.trace import CommandTracer
+from opencensus.trace.propagation import binary_format
+from opencensus.trace.tracers.noop_tracer import NoopTracer
 
 log = logging.getLogger(__name__)
 
-MODULE_NAME = 'motor_tornado'
+MODULE_NAME = 'pymongo_tornado'
 
 
 def trace_integration(tracer=None):
     """Wrap the pymongo connection to trace it."""
     log.info('Integrated module: {}'.format(MODULE_NAME))
-    monitoring.register(TornadoCommandTracer())
+    monitoring.register(CommandTracer())
+
+    # When running Pymongo under Tornado, all commands are run on a different thread pool. In order to get the correct
+    # context for the listener, we need to pass the context across the thread boundaries using some of the code from
+    # the 'threading' integration. We also could just use the 'threading' integration, but we don't want to start a
+    # span for every thread.
+    submit_func = getattr(futures.ThreadPoolExecutor, "submit")
+    setattr(
+        futures.ThreadPoolExecutor,
+        submit_func.__name__,
+        wrap_submit(submit_func),
+    )
 
 
-class TornadoCommandTracer(CommandTracer):
+def wrap_submit(submit_func):
+    """Wrap the apply_async function of multiprocessing.pools. Get the function
+    that will be called and wrap it then add the opencensus context."""
 
-    def started(self, event):
-        IOLoop.instance().add_callback(lambda: super(TornadoCommandTracer, self).started(event))
+    def call(self, func, *args, **kwargs):
+        wrapped_func = wrap_task_func(func)
+        _tracer = execution_context.get_opencensus_tracer()
+        propagator = binary_format.BinaryFormatPropagator()
 
-    def succeeded(self, event):
-        IOLoop.instance().add_callback(lambda: super(TornadoCommandTracer, self).succeeded(event))
+        wrapped_kwargs = {}
+        wrapped_kwargs["span_context_binary"] = propagator.to_header(
+            _tracer.span_context
+        )
+        wrapped_kwargs["kwds"] = kwargs
 
-    def failed(self, event):
-        IOLoop.instance().add_callback(lambda: super(TornadoCommandTracer, self).failed(event))
+        if isinstance(_tracer, NoopTracer):
+            wrapped_kwargs["noop"] = True
+        else:
+            wrapped_kwargs["sampler"] = _tracer.sampler
+            wrapped_kwargs["exporter"] = _tracer.exporter
+            wrapped_kwargs["propagator"] = _tracer.propagator
+
+        return submit_func(self, wrapped_func, *args, **wrapped_kwargs)
+
+    return call
+
+
+class wrap_task_func(object):
+    """Wrap the function given to apply_async to get the tracer from context,
+    execute the function then clear the context."""
+
+    def __init__(self, func):
+        self.func = func
+
+    def __call__(self, *args, **kwargs):
+        kwds = kwargs.pop("kwds")
+
+        span_context_binary = kwargs.pop("span_context_binary")
+        propagator = binary_format.BinaryFormatPropagator()
+        kwargs["span_context"] = propagator.from_header(span_context_binary)
+
+        _tracer = NoopTracer() if kwargs.get("noop", False) else tracer.Tracer(**kwargs)
+        execution_context.set_opencensus_tracer(_tracer)
+        result = self.func(*args, **kwds)
+        return result
